1442b81c21f581b545a0f918ce09364a451e61b9
elise <115508722+cultelise@users.noreply.github.com>
2022-11-07 06:22:35 -0600

    Safe commit

--- src/add.ts
+++ src/add.ts
@@ -1,46 +1,30 @@
-import "./style.css"
+import { createProject } from "./create";
+import { displayProject } from "./display";
+import "./variable"
+
 
 const projectList = document.getElementById("projectList") as HTMLDivElement;
 const projectButton = document.getElementById('addProjectButton') as HTMLButtonElement;
 const tasks = document.getElementById('tasks') as HTMLDivElement;
 const taskButton = document.getElementById('addTaskButton') as HTMLButtonElement;
-console.log(projectButton)
 
 document.addEventListener('DOMContentLoaded', () => {
-  projectButton?.addEventListener('click', newProjectInput)
-  taskButton?.addEventListener('click', () => console.log(1))
+  projectButton.addEventListener('click', newProjectInput)
+  taskButton.addEventListener('click', () => console.log(1))
 })
 
-const projects: object[] = [];
 let counter = 0;
 
 const newProjectInput = () => {
-  console.log(1);
-  const newInput: HTMLInputElement = document.createElement(`input`);
-  newInput.classList.add(`input${counter}`);
-  newInput.addEventListener('change', () => {inputExit(); console.log(projects)});
-  console.log(newInput)
-  projectList?.appendChild(newInput);
+  const titleInput = createProject();
+  projectList?.appendChild(titleInput);
+  titleInput.focus();
 }
 
-const inputExit = () => {
-  const inputElement = document.querySelector(`.input${counter}`) as HTMLInputElement;
-  const inputValue = inputElement.value;
-  inputElement?.remove();
-  AddProject(inputValue);
-  return projects.push(newProject(inputValue.toString()))
-}
 
-const newProject = (input:string) => {
- return { title: input}
-}
-
-const AddProject = (inputValue:string) => {
+export const addProjectTitle = (inputValue:string) => {
   const inputLabel = document.createElement('div')
   inputLabel.textContent = inputValue;
+  displayProject(inputLabel);
   projectList?.appendChild(inputLabel);
-}
-
-function checkList() {
-  
 }
